e64a56a26a7ba01e627e8d8de8ae57ac769224ed
Jeff Lbs <jefflbs@proton.me>
2025-01-24 05:22:07 -0500

    Add Button page gamedev & fix subtitle

--- content.js
+++ content.js
@@ -1,29 +1,75 @@
-let subtitleContainer = document.querySelector('div.plyr__captions');
+/**
+ *
+ * ADD button page gamedev
+ *
+ */
+const observer = new MutationObserver(() => {
+  const targetSection = document.querySelector('section.sticky.top-0');
+  const iframe = document.querySelector('iframe.absolute');
 
-if (subtitleContainer) {
-  let config = { childList: true, subtree: true };
+  if (!document.querySelector('button.buttonIframe')) {
+    const button = document.createElement('button');
+    button.className = 'buttonIframe';
+    button.textContent = 'Open Iframe';
+    button.style.margin = '10px';
+    button.style.padding = '5px 10px';
+    button.style.cursor = 'pointer';
+    button.style.width = '10%';
 
-  var callback = async function (mutationsList) {
-    for (var mutation of mutationsList) {
-      if (mutation.type === 'childList') {
-        mutation.addedNodes.forEach(async function (node) {
-          if (node.matches && node.matches('span.plyr__caption')) {
-            // console.log('🚀 ~ node:', node.innerText);
-            const translateText = await fetchTranslation(node.innerText);
-            // console.log(
-            //   '🚀 ~ translateText.translatedText:',
-            //   translateText.translatedText
-            // );
-            node.innerHTML = translateText.translatedText;
-          }
-        });
+    button.addEventListener('click', () => {
+      window.open(iframe.src, '_blank');
+    });
+
+    targetSection.appendChild(button);
+    observer.disconnect();
+  }
+});
+
+observer.observe(document.body, { childList: true, subtree: true });
+
+/**
+ *
+ * Translate subtitle IFrame
+ *
+ */
+function observeSubtitleContainer() {
+  const subtitleContainer = document.querySelector('div.plyr__captions');
+
+  if (subtitleContainer) {
+    let config = { childList: true, subtree: true };
+
+    var callback = async function (mutationsList) {
+      for (var mutation of mutationsList) {
+        if (mutation.type === 'childList') {
+          mutation.addedNodes.forEach(async function (node) {
+            if (node.matches && node.matches('span.plyr__caption')) {
+              // console.log('🚀 ~ node:', node.innerText);
+              const translateText = await fetchTranslation(node.innerText);
+              // console.log(
+              //   '🚀 ~ translateText.translatedText:',
+              //   translateText.translatedText
+              // );
+              node.innerHTML = translateText.translatedText;
+            }
+          });
+        }
       }
-    }
-  };
+    };
 
-  var observer = new MutationObserver(callback);
-  observer.observe(subtitleContainer, config);
+    var observer = new MutationObserver(callback);
+    observer.observe(subtitleContainer, config);
+  }
 }
+
+const observeGlobal = new MutationObserver(() => {
+  const subtitleContainer = document.querySelector('div.plyr__captions');
+  if (subtitleContainer) {
+    observeSubtitleContainer();
+    observeGlobal.disconnect();
+  }
+});
+
+observeGlobal.observe(document.body, { childList: true, subtree: true });
 
 async function fetchTranslation(text) {
   const modifiedText = modifyText(text);
@@ -46,15 +92,19 @@ async function fetchTranslation(text) {
 function modifyText(text) {
   const replace = {
     unreal: 'UNREAL',
+    engine: 'ENGINE',
     blueprint: 'BLUEPRINT',
     portfolio: 'PORTFOLIO',
   };
 
-  let modifiedText = text;
-  for (const [key, value] of Object.entries(replace)) {
-    const regex = new RegExp(`\\b${key}\\b`, 'gi');
-    modifiedText = modifiedText.replace(regex, value);
-  }
+  let words = text.split(' ');
+  words = words.map((word) => {
+    const lowerCaseWord = word.toLowerCase();
+    if (replace[lowerCaseWord]) {
+      return replace[lowerCaseWord];
+    }
+    return word;
+  });
 
-  return modifiedText;
+  return words.join(' ');
 }
